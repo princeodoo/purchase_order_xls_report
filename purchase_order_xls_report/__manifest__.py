934ad4166c81df6e5af4c6703b4678df5bfcea02
Prince <prince.odoo.up@gmail.com>
2018-09-30 23:58:15 +0530

    Modified

--- purchase_order_xls_report/__manifest__.py
+++ purchase_order_xls_report/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': '''
         Prints Excel Report based on purchase order status and vendor.
         ''',
-    'author': 'Prince',
+    'author': 'HK',
     'license': "OPL-1",
     'depends': [
         'purchase'
